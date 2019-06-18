4ff5f742a3c90ce2fc2c3da01d40c951ad16e47e
Jasonth23 <jasonth@hotmail.es>
2019-06-18 11:30:18 -0600

    Add files via upload
    Consola

---
+++ Consola/src/consola/app.java
@@ -0,0 +1,16 @@
+package consola;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jason
+ */
+interface app {
+    
+    public String SayMessage (String MyMessage);
+}
